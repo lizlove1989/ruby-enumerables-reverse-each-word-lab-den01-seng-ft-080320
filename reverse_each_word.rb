99cb24c889815f53dc613d89f96dfd65bd70bcdd
Liz Love <love.elizabeth1989@gmail.com>
2020-06-19 20:50:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
-def reverse_each_word(sentence1)
-  sentence1.split(/ /)
-  puts sentence1
+def reverse_each_word(sentence)
+  array1 = sentence.split(" ")
+  array2 = []
+  array1.each {|string| array2 << string.reverse }
+  array2.join(" ")
 end
