41411176d14494ef6afd08ba1693136b6ca4beb4
Liz Love <love.elizabeth1989@gmail.com>
2020-06-19 20:09:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,5 +4,5 @@ def reverse_each_word(string1)
   string1 = string1.split(/ /)
   string1.reverse_each {|element| print element " "}
   string1.collect {|element| element}
-  puts string1
+  binding.pry
 end
