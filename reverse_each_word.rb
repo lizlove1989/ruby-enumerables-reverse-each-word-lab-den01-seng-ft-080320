4b94af25544e6ba5238a9a24982af04887205d23
Liz Love <love.elizabeth1989@gmail.com>
2020-06-19 20:20:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 def reverse_each_word(sentence1)
-  # sentence1 = sentence1.split(/ /)
-  sentence1.reverse_each{|x| print x, " " }
+  sentence1 = sentence1.split(/ /)
+  # sentence1.reverse_each{|x| print x, " " }
+  puts sentence1
 end
